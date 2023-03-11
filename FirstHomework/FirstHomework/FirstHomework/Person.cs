1cde781280c06a61211160c931fc0282e581899d
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2023-03-11 20:05:38 +0300

    add first homework

---
+++ FirstHomework/FirstHomework/FirstHomework/Person.cs
@@ -0,0 +1,31 @@
+﻿
+namespace FirstHomework
+{
+    // internal - виден внутри сборки
+    internal class Person
+    {
+        // private - вмден внутри класса
+        private string _name;
+
+        // свойство с именем
+        // свойство = методы
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                // value - передоваемый параметр
+                _name = value;
+            }
+        }
+        public Person(string name)
+        {
+            _name = name;
+        }
+
+    }
+}
