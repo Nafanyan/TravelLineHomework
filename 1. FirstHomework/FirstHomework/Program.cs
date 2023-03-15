69cc7daaf0024473fcd845bbb641b66025a5b04c
lya aleksandrov <01.11.2001-01.11.2001@mail.ru>
2023-03-15 11:34:02 +0300

    swap place dir

---
+++ 1. FirstHomework/FirstHomework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FirstHomework
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            string nameStudent = "Igor";
+            Person student = new Person(nameStudent);
+            student.Name = "Ilya";
+            Console.WriteLine(student.Name);
+        }
+    }
+}
